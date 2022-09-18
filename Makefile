b300a9b01ba421a2401d55623bcc3e3a5d031f5a
Peter Burgess <pburgess2003@gmail.com>
2022-09-19 01:42:01 +0930

    Annotations for easier reading, moved sfml test into a different file, main file for project added

--- Makefile
+++ Makefile
@@ -1,7 +1,26 @@
-all: compile link
 
-compile:
-	g++ -Isrc/include -c main.cpp
 
-link:
-	g++ main.o -o main -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
+# The main make command for the project, use `make` to compile and `make run` to run the executable
+all: main-compile main-link
+
+main-compile:
+	g++ -Isrc/include -c main.cpp -o "object files/main.o"
+
+main-link:
+	g++ "object files/main.o" -o "executables/main" -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
+
+run:
+	./"executables/main.exe"
+
+
+# Simple main file for testing if sfml works, this is for windows computers
+sfml: compile-sfml link-sfml
+
+compile-sfml:
+	g++ -Isrc/include -c sfml-test.cpp -o "object files/sfml-test.o"
+
+link-sfml:
+	g++ "object files/sfml-test.o" -o "executables/sfml-test" -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
+
+sfml-run:
+	./"executables/sfml-test.exe"
--- main.cpp
+++ main.cpp
@@ -1,24 +1,54 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    cout << "\n========== Program Start ==========\n"
+         << endl;
+
+    // Create the window
+    sf::RenderWindow window(
+        sf::VideoMode(1280, 720),
+        "OOP Group Project");
+
+    // Circle
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    // Where the event being handled is stored
+    sf::Event event;
+
     while (window.isOpen())
     {
-        sf::Event event;
+        // Event handler
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            switch (event.type)
+            {
+            case sf::Event::Closed: // Window closed
                 window.close();
+                break;
+
+            case sf::Event::KeyPressed: // Escape key pressed
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close();
+                break;
+            }
         }
 
+        // Drawing stuff
         window.clear();
         window.draw(shape);
+
+        // WOOO
         window.display();
     }
+
+    cout << "\n=============== End ===============\n"
+         << endl;
 
     return 0;
 }
