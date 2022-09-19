4cbf3a803b36928249e914b17b1a4d89aa64981b
Peter Burgess <pburgess2003@gmail.com>
2022-09-19 20:03:31 +0930

    File movement for neatness. New movement test as a proof of concept. Removed uneccessary code from main file

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ main-compile:
 	g++ -Isrc/include -c main.cpp -o "object files/main.o"
 
 main-link:
-	g++ "object files/main.o" -o "executables/main" -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
+	g++ "object files/main.o" -o "executables/main.exe" -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
 
 run:
 	./"executables/main.exe"
@@ -17,10 +17,23 @@ run:
 sfml: compile-sfml link-sfml
 
 compile-sfml:
-	g++ -Isrc/include -c sfml-test.cpp -o "object files/sfml-test.o"
+	g++ -Isrc/include -c "tests/sfml-test.cpp" -o "object files/sfml-test.o"
 
 link-sfml:
-	g++ "object files/sfml-test.o" -o "executables/sfml-test" -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
+	g++ "object files/sfml-test.o" -o "executables/sfml-test.exe" -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
 
 sfml-run:
 	./"executables/sfml-test.exe"
+
+
+# A test for moving an object around the screen
+movement: compile-movement link-movement
+
+compile-movement:
+	g++ -Isrc/include -c "tests/movement-test.cpp" -o "object files/movement-test.o"
+
+link-movement:
+	g++ "object files/movement-test.o" -o "executables/movement-test.exe" -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
+
+movement-run:
+	./"executables/movement-test.exe"
--- main.cpp
+++ main.cpp
@@ -10,18 +10,13 @@ int main()
          << endl;
 
     // Create the window
-    sf::RenderWindow window(
-        sf::VideoMode(1280, 720),
-        "OOP Group Project");
+    sf::RenderWindow window(sf::VideoMode(1280, 720),
+                            "OOP Group Project");
 
     // Circle
-    sf::CircleShape shape(100.f); // Radius
+    sf::CircleShape shape(100.f);         // Radius
     shape.setFillColor(sf::Color::Green); // Color
-    shape.setPosition(100.f, 100.f); // Position
-
-    // For movement of the shape
-    float x_movement;
-    float y_movement;
+    shape.setPosition(100.f, 100.f);      // Position
 
     // Where the event being handled is stored
     sf::Event event;
@@ -43,20 +38,6 @@ int main()
                 break;
             }
         }
-
-        // Moving the shape
-        x_movement = 0.f;
-        y_movement = 0.f;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            x_movement -= 1.f;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            x_movement += 1.f;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            y_movement -= 1.f;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            y_movement += 1.f;
-
-        shape.move(x_movement, y_movement);
 
         // Drawing stuff
         window.clear();
