c9b7c6d82d79146076292c30d0a50bb74d12083e
Peter Burgess <pburgess2003@gmail.com>
2022-10-10 10:15:25 +1030

    Better test to find bugs. There are bugs in the collision solving, that is what I will be working on.

--- hitbox1-test.cpp
+++ hitbox1-test.cpp
@@ -6,16 +6,15 @@ using std::endl;
 using std::sqrt;
 
 #include <SFML/Graphics.hpp>
-using sf::RenderWindow;
 using sf::CircleShape;
-using sf::RectangleShape;
 using sf::Color;
+using sf::RectangleShape;
+using sf::RenderWindow;
 
 #include <SFML/System.hpp>
 using sf::Vector2f;
 
 #include "GameLibrary/GameLibrary.h"
-
 
 int main()
 {
@@ -31,14 +30,52 @@ int main()
 
     // Entities to play with
     Entity unmoving;
-    unmoving.body.setPosition(100.f, 100.f);
+    unmoving.body.setPosition(300.f, 250.f);
     unmoving.body.setSize(Vector2f(100.f, 100.f));
 
-    Entity moving;
-    moving.body.setPosition(1000.f, 125.f);
-    moving.body.setSize(Vector2f(50.f, 50.f));
-    moving.body.setFillColor(Color(255, 125, 125));
-    moving.changeVelocity(-1.f, 0.f);
+    vector<Entity> moving_entities;
+
+    for (int x = 0; x < 3; x++)
+    {
+        for (int y = 0; y < 2; y++)
+        {
+            Entity temp;
+            temp.body.setSize(Vector2f(50.f, 50.f));
+
+            temp.body.setPosition(200.f + x * 125.f,
+                                  100.f + 350.f * y);
+
+            if (y)
+                temp.body.setFillColor(Color::Red);
+            else
+                temp.body.setFillColor(Color::Magenta);
+
+            temp.changeVelocity(0.f, 1.f - 2.f * y);
+
+            moving_entities.push_back(temp);
+        }
+    }
+
+    for (int y = 0; y < 3; y++)
+    {
+        for (int x = 0; x < 2; x++)
+        {
+            Entity temp;
+            temp.body.setSize(Vector2f(50.f, 50.f));
+
+            temp.body.setPosition(150.f + 350.f * x,
+                                  150.f + y * 125.f);
+
+            if (x)
+                temp.body.setFillColor(Color::Yellow);
+            else
+                temp.body.setFillColor(Color::Cyan);
+            
+            temp.changeVelocity(1.f - 2.f * x, 0.f);
+
+            moving_entities.push_back(temp);
+        }
+    }
 
     // Where the event being handled is stored
     sf::Event event;
@@ -65,23 +102,43 @@ int main()
         }
 
         // Updates here
-        
+
         // Collecting hitboxes
         vector<RectangleShape> temp;
         temp.push_back(unmoving.body);
 
         // Moving
-        moving.move(temp);
+        vector<RectangleShape> hitboxes;
+        hitboxes.push_back(unmoving.body);
+        for (Entity &moving : moving_entities)
+        {
+            moving.move(hitboxes);
+        }
 
         // Drawing stuff
         window.clear();
 
         window.draw(unmoving.body);
-        window.draw(moving.body);
+        for (Entity &moving : moving_entities)
+        {
+            window.draw(moving.body);
+        }
 
         // WOO
         window.display();
     }
+
+    cout << "Unmoving: "
+         << unmoving.body.getPosition().x
+         << ", "
+         << unmoving.body.getPosition().y
+         << endl;
+
+    cout << "Moving:   "
+         << moving_entities[8].body.getPosition().x
+         << ", "
+         << moving_entities[8].body.getPosition().y
+         << endl;
 
     cout << "\n=============== End ===============\n"
          << endl;
