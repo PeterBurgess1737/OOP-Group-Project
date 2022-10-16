3c9304ad6020ae3760db6ebee7b57f499b770e5b
Caitlin Anchor <anchorcaitlin03@gmail.com>
2022-10-16 18:16:29 +1030

    add

--- mainmenu1-test.cpp
+++ mainmenu1-test.cpp
@@ -5,14 +5,16 @@
 using namespace std;
 
 int main() {
-    // Create an object of the class
 
+    // Create an object of the class
     TestMenu *menu = new TestMenu;
 
+    // Test draw main menu with high score being 56
     menu->draw_main_menu(56);
     cout << menu->get_main_menu_choice() << endl;
 
-    menu->draw_end_menu(56);
+    // Test draw end menu with current score being 56 
+    menu->draw_end_menu(56, 78);
     cout << menu->get_end_menu_choice() << endl;
 
     return 0;
